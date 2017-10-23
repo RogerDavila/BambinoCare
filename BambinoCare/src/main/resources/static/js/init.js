3399a28e18fef0fa3418b249b0de6aa720a9ba1b
DavidCruz168 <davidfernando168@gmail.com>
2017-10-22 23:44:26 -0500

    se agregan los templates y los recursos estaticos

---
+++ BambinoCare/src/main/resources/static/js/init.js
@@ -0,0 +1,66 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+    $('.parallax').parallax();
+
+    $('.pushpin-demo-nav').pushpin({
+      top: 30,
+      offset: 0
+    });
+
+    $('select').material_select();
+
+    ///User Section
+    $("#save").hide();
+    $("#cancel").hide();
+    $("#editPerfil").click(function(){
+      $(":disabled").prop("disabled", false);
+      $("#save").show();
+      $("#cancel").show();
+    });
+    $("#cancel").click(function(){
+      location.reload(true);
+    });
+
+    $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15, // Creates a dropdown of 15 years to control year,
+      today: 'Today',
+      clear: 'Clear',
+      close: 'Ok',
+      closeOnSelect: false // Close upon selecting a date,
+    });
+    $('.timepicker').pickatime({
+      default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+      fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+      twelvehour: false, // Use AM/PM or 24-hour format
+      donetext: 'OK', // text for done-button
+      cleartext: 'Clear', // text for clear-button
+      canceltext: 'Cancel', // Text for cancel-button
+      autoclose: false, // automatic close timepicker
+      ampmclickable: true, // make AM PM clickable
+      aftershow: function(){} //Function for after opening timepicker
+    });
+
+    //Reservacion
+    $("#Tutorias").hide();
+    $("#Evento").hide();
+    $('#tipoServicio').change(function(){ 
+    	if($(this).val() == 2){
+        $("#Tutorias").hide();
+        $("#Evento").show();
+    	}
+    	if($(this).val() == 3){
+        $("#Evento").hide();
+        $("#Tutorias").show();
+    	}
+	});
+    
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+
