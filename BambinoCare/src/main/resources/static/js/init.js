040568a291c2c271417c2258a440d3936a1e26dc
RogerDavila <rog.davila94@gmail.com>
2017-12-19 02:42:19 -0600

    Mensaje de aviso por 4 bambino

--- BambinoCare/src/main/resources/static/js/init.js
+++ BambinoCare/src/main/resources/static/js/init.js
@@ -113,7 +113,7 @@
       window.location.href = "http://localhost:8080/#Nosotros";
     });
     
-    $('.modal').modal({
+    $('#paymentmodal').modal({
         dismissible: true, // Modal can be dismissed by clicking outside of the modal
         opacity: .5, // Opacity of modal background
         //inDuration: 300, // Transition in duration
@@ -126,6 +126,20 @@
         },
         complete: function() { 
         	document.forms['bookingform'].submit();
+        }
+      }
+    );
+    
+    $('#bambinomodal').modal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .5, // Opacity of modal background
+        inDuration: 300, // Transition in duration
+        outDuration: 200, // Transition out duration
+        startingTop: '4%', // Starting top style attribute
+        endingTop: '10%', // Ending top style attribute
+        ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+        },
+        complete: function() { 
         }
       }
     );
