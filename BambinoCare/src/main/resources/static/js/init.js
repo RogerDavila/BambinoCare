a83bd452430da2239ed11f2e05b2e2089bf8c22c
RogerDavila <rog.davila94@gmail.com>
2018-01-25 19:47:45 -0600

    Se setea idioma en español del datepicker

--- BambinoCare/src/main/resources/static/js/init.js
+++ BambinoCare/src/main/resources/static/js/init.js
@@ -42,11 +42,11 @@
       fromnow: 0,       // set default time to * milliseconds from now (using
 						// with default = 'now')
       twelvehour: false, // Use AM/PM or 24-hour format
-      donetext: 'OK', // text for done-button
-      cleartext: 'Clear', // text for clear-button
-      canceltext: 'Cancel', // Text for cancel-button
+      donetext: 'Aceptar', // text for done-button
+      cleartext: 'Limpiar', // text for clear-button
+      canceltext: 'Cancelar', // Text for cancel-button
       autoclose: false, // automatic close timepicker
-      ampmclickable: true, // make AM PM clickable
+      ampmclickable: false, // make AM PM clickable
       aftershow: function(){} // Function for after opening timepicker
     });
 
